a850be525b8928f9c2416b34696708e73bfa2be9
mohammad-rezaei77 <45556418+mohammad-rezaei77@users.noreply.github.com>
2024-10-03 11:12:01 +0330

    deployment database

--- app/main.py
+++ app/main.py
@@ -13,7 +13,15 @@ from scraper.session import insta_create_session, save_session
 app = FastAPI()
 
 """register models in the database"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+from app.database import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 # Base.metadata.create_all(bind=engine)
 """////////////////////////////////"""
 

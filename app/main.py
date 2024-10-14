7b1fb7155e0900425b5605420669efb28659ad45
mohammad-rezaei77 <45556418+mohammad-rezaei77@users.noreply.github.com>
2024-10-14 09:18:11 +0330

    a

--- app/main.py
+++ app/main.py
@@ -8,12 +8,12 @@ app.include_router(session_views.router)
 app.include_router(post_views.router)
 
 
-"""
+# """
 from .database import Base, engine
 
 # Create a detabase
 Base.metadata.create_all(bind=engine)
-"""
+# """
 
 
 @app.get("/")
